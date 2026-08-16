b8ae69ccc132d9b90dba7cffee16f707f4eb052f
ALEXIS <ing.parradiaz@gmail.com>
2026-08-16 16:07:55 -0600

    ultimos cambios de database anted del deploy

--- backend/database.py
+++ backend/database.py
@@ -1,7 +1,14 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:team20@localhost:5432/economia_db"
+SQLALCHEMY_DATABASE_URL = os.environ.get(
+    "DATABASE_URL",
+    "postgresql://postgres:team20@localhost:5432/economia_db"
+)
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
